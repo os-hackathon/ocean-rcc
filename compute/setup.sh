8e6d903cf7553688523698173a9015fdd179c21b
Joe Schoonover <joe@fluidnumerics.com>
2021-04-04 22:06:10 +0000

    Add CUDNN to compute node images

--- compute/setup.sh
+++ compute/setup.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-# TO DO : Add instructions for installing your applications
+
+# Disable repos that are currently causing issues with yum
+yum install -y yum-utils
+yum-config-manager --disable gcsfuse
+yum-config-manager --disable google-cloud-logging
+yum-config-manager --disable google-cloud-monitoring
+yum-config-manager --disable google-cloud-sdk
+yum-config-manager --disable google-compute-engine
+yum clean all
+yum -y update
+
+# Install cudnn
+yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.repo
+yum clean all
+yum -y update
+yum install -y libcudnn8
+yum install -y libcudnn8-devel
+
+
