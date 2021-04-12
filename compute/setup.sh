ad5ad0854e3c43b6451cd20485e622786d61a31e
Joe Schoonover <joe@fluidnumerics.com>
2021-04-12 21:56:12 +0000

    Force ROCm repo to use rocm-4.0.1

--- compute/setup.sh
+++ compute/setup.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-
-# Disable repos that are currently causing issues with yum
 yum install -y yum-utils
-yum-config-manager --disable gcsfuse
-yum-config-manager --disable google-cloud-logging
-yum-config-manager --disable google-cloud-monitoring
-yum-config-manager --disable google-cloud-sdk
-yum-config-manager --disable google-compute-engine
-yum clean all
-yum -y update
-
 # Install cudnn
 yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
 yum clean all
@@ -18,4 +8,21 @@ yum -y update
 yum install -y libcudnn8
 yum install -y libcudnn8-devel
 
+# Install OpenCV
+yum install -y opencv opencv-devel opencv-python
+
+
+# Revert to ROCm 4.0.1
+yum remove -y rocm-dev
+cat > /etc/yum.repos.d/rocm.repo  << EOL
+[ROCm]
+name=ROCm
+baseurl=https://repo.radeon.com/rocm/yum/4.0.1
+enabled=1
+gpgcheck=1
+gpgkey=https://repo.radeon.com/rocm/rocm.gpg.key
+EOL
+yum clean all
+yum update -y
+yum install -y rocm-dev
 
--- controller/setup.sh
+++ controller/setup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 # TO DO : Add instructions for installing your applications
-
-source /apps/spack/share/spack/setup-env.sh
-spack install hipify-clang
