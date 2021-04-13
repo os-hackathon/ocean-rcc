acea830860f0dba2db889ccd0e18993012894f8b
Joe Schoonover <joe@fluidnumerics.com>
2021-04-13 00:27:33 +0000

    Remove call to yum update
    * The yum update call updates the nvidia drivers but their is an
    incompatibility with the linux kernel that causes nvidia-smi to fail

--- compute/setup.sh
+++ compute/setup.sh
@@ -4,7 +4,6 @@ yum install -y yum-utils
 # Install cudnn
 yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
 yum clean all
-yum -y update
 yum install -y libcudnn8
 yum install -y libcudnn8-devel
 
